288c51322838dc6dc417bcfc741291329c7db52c
Luis Capelo <luiscape@gmail.com>
2015-04-25 13:06:52 -0400

    initial commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HDX_KEY=""
+
+# Activating the DataStore
+source venv/bin/activate
+python tool/code/create_datastore.py $HDX_KEY tool/data/temp.csv
+# python tool/code/create_datastore_stag.py $HDX_KEY tool/data/temp.csv
